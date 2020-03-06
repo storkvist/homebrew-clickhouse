20865eb3c56157e6d33ec8723ecddf73a86de22e
Vitaly Shlyaga <vitaly@shlyaga.ru>
2020-03-06 17:59:54 +0300

    .
    Change-Id: Icf116a455ca1ffc36345ed83ea0aa63f5ffd0839

--- Formula/clickhouse.rb
+++ Formula/clickhouse.rb
@@ -1,10 +1,9 @@
 class Clickhouse < Formula
   desc "ClickHouse — open source distributed column-oriented DBMS"
   homepage "https://clickhouse.tech"
-  url "https://github.com/ClickHouse/ClickHouse/archive/v20.3.1.2564-testing.tar.gz"
-  version "20.3.1.2564"
-
-  head "https://github.com/yandex/ClickHouse.git"
+  version "20.1.6.30"
+  sha256 "935524456f2291afa36ef815e68f1ab4a37a4ed6f0f144b7de7fb270733e13af"
+  url "https://github.com/yandex/ClickHouse.git", tag: "v20.1.6.30-stable"
 
   depends_on "cmake" => :build
   depends_on "ninja" => :build
