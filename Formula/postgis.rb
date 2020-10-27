1ce746e1adadb39b395f580a50138bf0caa7e5e6
Vitaly Shlyaga <vitaly@shlyaga.ru>
2020-10-27 13:31:31 +0300

    postgis

---
+++ Formula/postgis.rb
@@ -0,0 +1,122 @@
+class Postgis < Formula
+  desc "Adds support for geographic objects to PostgreSQL"
+  homepage "https://postgis.net/"
+  url "https://download.osgeo.org/postgis/source/postgis-3.0.2.tar.gz"
+  sha256 "a3a1641dfd73c83924088a185bdb8b35567b3d1dc8d0887f9e4b492e228ab2ca"
+  license "GPL-2.0-or-later"
+  revision 1
+
+  livecheck do
+    url "https://download.osgeo.org/postgis/source/"
+    regex(/href=.*?postgis[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    cellar :any
+    sha256 "d018b61597193794de49bb3a6c16c1d89dcf59e61b6778fe37ab1a2a7869be76" => :catalina
+    sha256 "50f0a33badd139f158e502da18aff29724a6827a5213307fa5b41453afc327c2" => :mojave
+    sha256 "a3a4b36847abded9c121d8786d39b12c4a54044df217675f49821178c74c3532" => :high_sierra
+  end
+
+  head do
+    url "https://git.osgeo.org/gitea/postgis/postgis.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "gpp" => :build
+  depends_on "pkg-config" => :build
+  depends_on "gdal" # for GeoJSON and raster handling
+  depends_on "geos"
+  depends_on "json-c" # for GeoJSON and raster handling
+  depends_on "pcre"
+  depends_on "postgresql@12"
+  depends_on "proj"
+  depends_on "protobuf-c" # for MVT (map vector tiles) support
+  depends_on "sfcgal" # for advanced 2D/3D functions
+
+  def install
+    ENV.deparallelize
+
+    args = [
+      "--with-projdir=#{Formula["proj"].opt_prefix}",
+      "--with-jsondir=#{Formula["json-c"].opt_prefix}",
+      "--with-pgconfig=#{Formula["postgresql@12"].opt_bin}/pg_config",
+      "--with-protobufdir=#{Formula["protobuf-c"].opt_bin}",
+      # Unfortunately, NLS support causes all kinds of headaches because
+      # PostGIS gets all of its compiler flags from the PGXS makefiles. This
+      # makes it nigh impossible to tell the buildsystem where our keg-only
+      # gettext installations are.
+      "--disable-nls",
+    ]
+
+    system "./autogen.sh" if build.head?
+    system "./configure", *args
+    system "make"
+
+    mkdir "stage"
+    system "make", "install", "DESTDIR=#{buildpath}/stage"
+
+    bin.install Dir["stage/**/bin/*"]
+    lib.install Dir["stage/**/lib/*"]
+    include.install Dir["stage/**/include/*"]
+    (doc/"postgresql/extension").install Dir["stage/**/share/doc/postgresql/extension/*"]
+    (share/"postgresql/extension").install Dir["stage/**/share/postgresql/extension/*"]
+    pkgshare.install Dir["stage/**/contrib/postgis-*/*"]
+    (share/"postgis_topology").install Dir["stage/**/contrib/postgis_topology-*/*"]
+
+    # Extension scripts
+    bin.install %w[
+      utils/create_undef.pl
+      utils/postgis_proc_upgrade.pl
+      utils/postgis_restore.pl
+      utils/profile_intersects.pl
+      utils/test_estimation.pl
+      utils/test_geography_estimation.pl
+      utils/test_geography_joinestimation.pl
+      utils/test_joinestimation.pl
+    ]
+
+    man1.install Dir["doc/**/*.1"]
+  end
+
+  test do
+    require "base64"
+    (testpath/"brew.shp").write ::Base64.decode64 <<~EOS
+      AAAnCgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAoOgDAAALAAAAAAAAAAAAAAAA
+      AAAAAADwPwAAAAAAABBAAAAAAAAAFEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+      AAAAAAAAAAAAAAAAAAEAAAASCwAAAAAAAAAAAPA/AAAAAAAA8D8AAAAAAAAA
+      AAAAAAAAAAAAAAAAAgAAABILAAAAAAAAAAAACEAAAAAAAADwPwAAAAAAAAAA
+      AAAAAAAAAAAAAAADAAAAEgsAAAAAAAAAAAAQQAAAAAAAAAhAAAAAAAAAAAAA
+      AAAAAAAAAAAAAAQAAAASCwAAAAAAAAAAAABAAAAAAAAAAEAAAAAAAAAAAAAA
+      AAAAAAAAAAAABQAAABILAAAAAAAAAAAAAAAAAAAAAAAUQAAAAAAAACJAAAAA
+      AAAAAEA=
+    EOS
+    (testpath/"brew.dbf").write ::Base64.decode64 <<~EOS
+      A3IJGgUAAABhAFsAAAAAAAAAAAAAAAAAAAAAAAAAAABGSVJTVF9GTEQAAEMA
+      AAAAMgAAAAAAAAAAAAAAAAAAAFNFQ09ORF9GTEQAQwAAAAAoAAAAAAAAAAAA
+      AAAAAAAADSBGaXJzdCAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgICAgIFBvaW50ICAgICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgU2Vjb25kICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgICAgICBQb2ludCAgICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgIFRoaXJkICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgICAgICAgUG9pbnQgICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgICBGb3VydGggICAgICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgICAgICAgIFBvaW50ICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgICAgQXBwZW5kZWQgICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgICAgICAgICBQb2ludCAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgICAg
+    EOS
+    (testpath/"brew.shx").write ::Base64.decode64 <<~EOS
+      AAAnCgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARugDAAALAAAAAAAAAAAAAAAA
+      AAAAAADwPwAAAAAAABBAAAAAAAAAFEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+      AAAAAAAAAAAAAAAAADIAAAASAAAASAAAABIAAABeAAAAEgAAAHQAAAASAAAA
+      igAAABI=
+    EOS
+    result = shell_output("#{bin}/shp2pgsql #{testpath}/brew.shp")
+    assert_match(/Point/, result)
+    assert_match(/AddGeometryColumn/, result)
+  end
+end
